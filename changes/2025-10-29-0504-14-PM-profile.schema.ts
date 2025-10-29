6b5370a5579c5cafbd6428f194c0d0b44c725bc7
DevTrack <devtrack@example.com>
2025-10-29 17:04:14 -0500

    DevTrack Update - October 29, 2025 at 5:04:14 PM GMT-5, 5:04:11 PM
    ✨ Feature development (Active coding time: 22 minutes, 1 file change events)
    
    Files: profile.schema.ts. [feature/JIRA-46-user-permissions-endpoint]
    Changes:
    - changed: profile.schema.ts (25 lines)
    
    Code Snippets:
    
    ```
    profile.schema.ts:
    import { EntitySchema } from 'typeorm';
    import { Profile } from 'src/domain/entities/profile.entity';
    
    export const ProfileSchema = new EntitySchema<Profile>({
        name: 'Profile',
        target: Profile,
        tableName: 'profile',
        columns: {
            id: { name: 'id', type: 'int', primary: true, generated: true },
            name: { name: 'name', type: 'varchar' },
            description: { name: 'description', type: 'varchar', nullable: true }
        },
        relations: {
            users: {
                type: 'one-to-many',
                target: 'User',
                inverseSide: 'profile',
            },
            profileGlobalRoles: {
                type: 'one-to-many',
                target: 'ProfileGlobalRole',
                inverseSide: 'profile',
            }
        }
    });
    ```
    
    Analysis:
    - Found feature keywords: new

---
+++ changes/2025-10-29-0504-14-PM-profile.schema.ts
@@ -0,0 +1,25 @@
+import { EntitySchema } from 'typeorm';
+import { Profile } from 'src/domain/entities/profile.entity';
+
+export const ProfileSchema = new EntitySchema<Profile>({
+    name: 'Profile',
+    target: Profile,
+    tableName: 'profile',
+    columns: {
+        id: { name: 'id', type: 'int', primary: true, generated: true },
+        name: { name: 'name', type: 'varchar' },
+        description: { name: 'description', type: 'varchar', nullable: true }
+    },
+    relations: {
+        users: {
+            type: 'one-to-many',
+            target: 'User',
+            inverseSide: 'profile',
+        },
+        profileGlobalRoles: {
+            type: 'one-to-many',
+            target: 'ProfileGlobalRole',
+            inverseSide: 'profile',
+        }
+    }
+});
